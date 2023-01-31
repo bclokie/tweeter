f7ae06828fa4834ee9d3df1b2e53b260e4751021
Braden <braden.clokie@gmail.com>
2023-01-31 17:45:24 -0500

    Capped the character count ⛔️

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,9 +3,12 @@ $(document).ready(function() {
   // >>> Change value of char counter using length of input.
   const $tweetText = $('#tweet-text');
   const $counter = $('.counter');
-
+  
   $($tweetText).on('input', function() {
+    if ($(this).val().length > 140) {
+      $(this).val($(this).val().substring(0, 140));
+    }
     $counter.val(140 - $(this).val().length);
   });
-
+  
 });
