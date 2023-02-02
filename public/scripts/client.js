44545ba3306cd58bbac61bf1883182dccdf10557
Braden <braden.clokie@gmail.com>
2023-02-02 00:00:58 -0500

    Added form validation ✔️

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,33 +6,6 @@
 
 $(document).ready(function() {
 
-  /*const tweetData = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];*/
-
-
   // >>> CREATE INDIVIDUAL TWEET ELEMENT FROM DB
   const createTweetElement = function(tweetObj) {
     const $tweetElement = `
@@ -46,8 +19,7 @@ $(document).ready(function() {
         </header>
         <p class="tweet-content">${tweetObj.content.text}</p>
         <footer>
-          <span>${tweetObj.created_at}</span>
-          <span class="tweet-action-symbols">
+        <span>${timeago.format(tweetObj.created_at)}</span>          <span class="tweet-action-symbols">
             <i class="fa-solid fa-flag"></i>
             <i class="fa-solid fa-retweet"></i>
             <i class="fa-solid fa-heart"></i>
@@ -72,8 +44,17 @@ $(document).ready(function() {
     event.preventDefault();
     const $formInput = $(this).serialize();
 
-    $.post('/tweets', $formInput);
-  });
+    // >>> FORM VALIDATION & POST REQUEST
+
+    const $tweetLength = $('#tweet-text').val().length;
+
+    if ($tweetLength === 0) {
+      alert("You didn't type anthing!");
+    } else if ($tweetLength > 140) {
+      alert("You typed too much!");
+    } else {
+      $.post('/tweets', $formInput);
+    }  });
 
   // >>> REQUEST TWEETS FROM DATABASE
   const loadTweets = function() {
